9613371909d8f3d8efa4ada4fbf37404f5ed1387
ahmad-musa <ahmadmusa0987@gmail.com>
2022-04-23 02:03:48 +0600

    1st Flight

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    console.log('Testing');
+
+    res.send('Checking Homepage');
+});
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
